113e201c9b2c8e52d9dd79ae950ed4701b5257c9
dbhinton <dhinton.us@gmail.com>
2021-09-11 03:10:08 -0400

    fixed html

--- main.js
+++ main.js
@@ -4,16 +4,16 @@
     // 2. User clicks desired cell
     // 3. What happens after user makes selection
 
-const boardValues = {
-    
+// constants
 
-}
 
 // Initialize state variables
 let scores;
 let winner;
 let results;
 let pieces;
+
+// Event Listners
 
 // Dom references
 const boardValue = []
