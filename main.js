4fc231ab3a9da4cdd662ff563840a99dc2230eac
dbhinton <dhinton.us@gmail.com>
2021-09-10 22:54:18 -0400

    fixed html (id's)

---
+++ main.js
@@ -0,0 +1,17 @@
+// 1. User clicks piece
+// 2. User clicks desired cell
+// 3. What happens after user 
+
+const boardValues = {
+    
+
+}
+
+// Initialize state variables
+let scores;
+let winner;
+let results;
+let pieces;
+
+// Dom references
+const boardValue = []
