4c619c29b8ac369f779cbb4f6fc0ce9dbd5ad0ef
dbhinton <dhinton.us@gmail.com>
2021-09-12 02:11:28 -0400

    added event listeners and player objects and changed html to more accurately represent checker board

--- main.js
+++ main.js
@@ -5,6 +5,9 @@
     // 3. What happens after user makes selection
 
 // constants
+const board = []
+
+
 
 
 // Initialize state variables
@@ -14,6 +17,47 @@ let results;
 let pieces;
 
 // Event Listners
+let orangeChecker = document.querySelectorAll(".orange-checker");
+let tanChecker = document.querySelectorAll(".tan-checker");
+const tiles = document.querySelectorAll('.tile');
+const whosGo = {
+    orangeGo:  document.querySelector("#orange-go"),
+    tanGo:  document.querySelector("tan-go")
+}
+
+const resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", function{
+    init();
+})
+
+// State variables
+let playerGo;
+let orangePieces;
+let tanPieces;
+let 
+
+
+// Player Properties
+const player = {
+    orange: {
+        numberOfPieces: 12,
+        userGo: true,
+        color: "orange"
+    },
+    tan: {
+        numberOfPieces: 12,
+        userGo: true
+        color: "tan"
+    }
+function userSelection(){
+    if()
+}
+
+// What happens when checker is clicked
+    // -Grab id of checker
+    // -Grab index of tile that the checker is on
+    // Is it a king?
+    // Possible moves
 
 // Dom references
 const boardValue = []
